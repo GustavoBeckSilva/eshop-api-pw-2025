383b8ee88aab8b7be43870a6324e11a6720eb6f7
GustavoBeckSilva <gustavogbks@gmail.com>
2025-06-06 07:01:59 -0300

    feat: Adiciona API de avaliações de produtos
    - Cria endpoints para consultar e adicionar avaliações.
    
    - Adiciona entidade, controller, use case e rotas para a funcionalidade.
    
    - Torna a rota de listagem de produtos (GET /produto) pública.

--- routes/rotas.js
+++ routes/rotas.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const { rotasCategorias }  = require('./rotasCategorias');
 const { rotasProdutos }  = require('./rotasProdutos');
+const { rotasAvaliacoes } = require('./rotasAvaliacoes');
 
 const rotas = new Router();
 
 rotas.use(rotasCategorias);
 rotas.use(rotasProdutos);
+rotas.use(rotasAvaliacoes);
 
 module.exports = rotas;
